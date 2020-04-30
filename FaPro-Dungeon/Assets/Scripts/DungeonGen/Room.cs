c4f2749f6bc121cd15fd90d71eec05c25b746a8e
skyblader <leandro.crespo@udo.edu>
2020-04-30 17:31:42 +0200

    dungeongen

---
+++ FaPro-Dungeon/Assets/Scripts/DungeonGen/Room.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+
+    public int Width;
+
+    public int Height;
+
+    public int X;
+
+    public int Y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(RoomController.instance == null)
+        {
+            Debug.Log("Wrong scene!");
+            return;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3(X * Width, Y * Height);
+    }
+}
