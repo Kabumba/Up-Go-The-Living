0cba241975a21416eaa27d3dcba0206d058c557c
skyblader <leandro.crespo@udo.edu>
2020-04-30 18:12:51 +0200

    dungeongen

--- FaPro-Dungeon/Assets/Scripts/DungeonGen/RoomController.cs
+++ FaPro-Dungeon/Assets/Scripts/DungeonGen/RoomController.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -41,6 +45,46 @@ public class RoomController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadRoom(string name, int x, int y)
+    {
+        RoomInfo newRoomData = new RoomInfo();
+        newRoomData.name = name;
+        newRoomData.X = x;
+        newRoomData.Y = y;
+
+        loadRoomQueue.Enqueue(newRoomData);
+    }
+
+    IEnumerator LoadRoomRoutine(RoomInfo info)
+    {
+        string roomName = currentWorldName + info.name;
+
+        AsyncOperation loadRoom = SceneManager.LoadSceneAsync(roomName, LoadSceneMode.Additive);
+
+        while(loadRoom.isDone == false)
+        {
+            yield return null;
+        }
+    }
+
+    public void RegisterRoom(Room room)
+    {
+        room.transform.position = new Vector3(
+                currentLoadRoomData.X * room.Width,
+                currentLoadRoomData.Y * room.Height,
+                0
+            );
+
+        room.X = currentLoadRoomData.X;
+        room.Y = currentLoadRoomData.Y;
+        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
+        room.transform.parent = transform;
+
+        isLoadingRoom = false;
+
+        loadedRooms.Add(room);
     }
 
     public bool DoesRoomExist( int x, int y)
