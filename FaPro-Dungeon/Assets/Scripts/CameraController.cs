c64890dd89e0ea016cf73f145dde2aa3d9d65b8e
skyblader <leandro.crespo@udo.edu>
2020-04-30 19:41:17 +0200

    cameraswitch

---
+++ FaPro-Dungeon/Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public static CameraController instance;
+
+    public Room currRoom;
+
+    public float moveSpeedRoomChange;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+        if(currRoom == null)
+        {
+            return;
+        }
+
+        Vector3 targetPos = getCameraTargetPosition();
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeedRoomChange);
+    }
+
+    Vector3 getCameraTargetPosition()
+    {
+        if(currRoom == null)
+        {
+            return Vector3.zero;
+        }
+
+        Vector3 targetPos = currRoom.GetRoomCentre();
+        targetPos.z = transform.position.z;
+
+        return targetPos;
+    }
+
+    public bool isSwitchingScene()
+    {
+        return transform.position.Equals(getCameraTargetPosition());
+    }
+}
