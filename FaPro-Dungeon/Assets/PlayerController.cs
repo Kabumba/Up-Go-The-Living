d7b6c6d8211f61637ca56fc7d6dedf00252a9cee
Kabumba <Freddy253@live.de>
2020-04-30 16:28:27 +0200

    Schießen

--- FaPro-Dungeon/Assets/PlayerController.cs
+++ FaPro-Dungeon/Assets/PlayerController.cs
@@ -6,7 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    public GameObject bulletPrefab;
+
+    public float bulletSpeed;
+
+    private float lastFire;
+
+    public float fireDelay;
+
     Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +28,31 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        float shootHorizontal = Input.GetAxis("ShootHorizontal");
+        float shootVertical = Input.GetAxis("ShootVertical");
+
+        if((shootHorizontal !=0 || shootVertical !=0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHorizontal,shootVertical);
+            lastFire = Time.time;
+        }
+
         //basic Movement bei dem man sich diagonal schneller bewegt
         //rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0); 
 
         float unadjustedSpeed = Mathf.Sqrt(horizontal * horizontal + vertical * vertical);
         float speedfactor = unadjustedSpeed==0 ? 0 : speed/unadjustedSpeed;
         rigidbody.velocity = new Vector3(horizontal * speedfactor, vertical * speedfactor, 0);
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab,transform.position,transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            x < 0 ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            y < 0 ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
+            );
     }
 }
