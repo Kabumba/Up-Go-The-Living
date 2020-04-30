857dc77d426578cd2fc6dee825ed4b25836165a5
Kabumba <Freddy253@live.de>
2020-04-30 16:50:15 +0200

    Schießen optimiert

--- FaPro-Dungeon/Assets/PlayerController.cs
+++ FaPro-Dungeon/Assets/PlayerController.cs
@@ -25,34 +25,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Inputs
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         float shootHorizontal = Input.GetAxis("ShootHorizontal");
         float shootVertical = Input.GetAxis("ShootVertical");
 
-        if((shootHorizontal !=0 || shootVertical !=0) && Time.time > lastFire + fireDelay)
-        {
-            Shoot(shootHorizontal,shootVertical);
-            lastFire = Time.time;
-        }
-
         //basic Movement bei dem man sich diagonal schneller bewegt
         //rigidbody.velocity = new Vector3(horizontal * speed, vertical * speed, 0); 
 
         float unadjustedSpeed = Mathf.Sqrt(horizontal * horizontal + vertical * vertical);
-        float speedfactor = unadjustedSpeed==0 ? 0 : speed/unadjustedSpeed;
+        float speedfactor = unadjustedSpeed == 0 ? 0 : speed / unadjustedSpeed;
         rigidbody.velocity = new Vector3(horizontal * speedfactor, vertical * speedfactor, 0);
+
+        //Schüsse nur Vertikal oder Horizontal
+        if (Time.time > lastFire + fireDelay)
+        {
+            if (shootHorizontal != 0)
+            {
+                Shoot(shootHorizontal, 0);
+            }
+            else
+                if (shootVertical != 0)
+                {
+                Shoot(0, shootVertical);
+                }
+        }
     }
 
     void Shoot(float x, float y)
     {
         GameObject bullet = Instantiate(bulletPrefab,transform.position,transform.rotation) as GameObject;
         bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        float fixedX = (x < 0) ? Mathf.Floor(x) : Mathf.Ceil(x);
+        float fixedY = (y < 0) ? Mathf.Floor(y) : Mathf.Ceil(y);
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
-            x < 0 ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
-            y < 0 ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            fixedX * bulletSpeed + rigidbody.velocity.x,
+            fixedY * bulletSpeed + rigidbody.velocity.y,
             0
             );
+        lastFire = Time.time;
     }
 }
