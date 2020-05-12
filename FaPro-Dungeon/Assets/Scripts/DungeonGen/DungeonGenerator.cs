3ce0e32c6d1bf4d8b11d074f12733d2cae2aaea7
Kabumba <Freddy253@live.de>
2020-05-12 23:03:03 +0200

    Layout generierung mit L-Systems

--- FaPro-Dungeon/Assets/Scripts/DungeonGen/DungeonGenerator.cs
+++ FaPro-Dungeon/Assets/Scripts/DungeonGen/DungeonGenerator.cs
@@ -6,18 +6,25 @@ public class DungeonGenerator : MonoBehaviour
 {
     public DungeonGenerationData dungeonGenerationData;
 
+    public LayoutGenerator Lg;
+
     private List<Vector2Int> dungeonRooms;
+
+    private Dictionary<Vector2Int, RoomNode> rooms;
+
 
     private void Start()
     {
-        dungeonRooms = DungeonCrawlerController.GenerateDungeon(dungeonGenerationData);
-        SpawnRooms(dungeonRooms);
+        //dungeonRooms = DungeonCrawlerController.GenerateDungeon(dungeonGenerationData);
+        rooms = Lg.GetLayout();
+
+        SpawnRooms(rooms);
     }
 
-    private void SpawnRooms(IEnumerable<Vector2Int> rooms)
+    private void SpawnRooms(Dictionary<Vector2Int, RoomNode> rooms)
     {
         RoomController.instance.LoadRoom("Start", 0, 0);
-        foreach(Vector2Int roomLocation in rooms)
+        foreach(Vector2Int roomLocation in rooms.Keys)
         {
             RoomController.instance.LoadRoom("Empty", roomLocation.x, roomLocation.y);
         }
--- FaPro-Dungeon/Assets/Scripts/DungeonGen/LayoutGenerator.cs
+++ FaPro-Dungeon/Assets/Scripts/DungeonGen/LayoutGenerator.cs
@@ -11,16 +11,25 @@ public enum RoomType
     Enemy,
 }
 
+public enum Direction
+{
+    up,
+    down,
+    left,
+    right
+}
+
 
 
 public class RoomNode
 {
-    RoomNode Up { get; set; }
-    RoomNode Down { get; set; }
-    RoomNode Left { get; set; }
-    RoomNode Right { get; set; }
-    RoomType Type { get; set; }
-    Vector2Int Position { get; set; }
+    public RoomNode Up { get; set; }
+    public RoomNode Down { get; set; }
+    public RoomNode Left { get; set; }
+    public RoomNode Right { get; set; }
+    public RoomType Type { get; set; }
+    public Vector2Int Position { get; set; }
+    public LayoutGenerator Lg;
 
     public RoomNode(RoomType t)
     {
@@ -33,6 +42,14 @@ public class RoomNode
         Position = pos;
     }
 
+    public static readonly Dictionary<Direction, Vector2Int> directionMovementMap = new Dictionary<Direction, Vector2Int>
+    {
+        {Direction.up, Vector2Int.up },
+        {Direction.left, Vector2Int.left },
+        {Direction.down, Vector2Int.down },
+        {Direction.right, Vector2Int.right }
+    };
+
     //Erzeugt einen neuen Raum vom Typ t in Richtung dir vom ausrufenden Raum aus.
     public RoomNode Create(RoomType t, Direction dir)
     {
@@ -42,56 +59,177 @@ public class RoomNode
             case Direction.up:
                 Up = newR;
                 newR.Down = this;
-                newR.Position = Position + Vector2Int.up;
                 break;
             case Direction.down:
                 Down = newR;
                 newR.Up = this;
-                newR.Position = Position + Vector2Int.down;
                 break;
             case Direction.left:
                 Left = newR;
                 newR.Right = this;
-                newR.Position = Position + Vector2Int.left;
                 break;
             case Direction.right:
                 Right = newR;
                 newR.Left = this;
-                newR.Position = Position + Vector2Int.right;
+                break;
+        }
+        newR.Position = Position + directionMovementMap[dir];
+        Lg.rooms.Add(newR.Position,newR);
+        Lg.roomList.Add(newR);
+        newR.Lg = Lg;
+
+        //Raumanzahl updaten
+        switch (t)
+        {
+            case RoomType.Loot:
+                Lg.numberOfLootRooms++;
+                break;
+            case RoomType.Boss:
+                Lg.numberOfBossRooms++;
+                break;
+            default:
+                Lg.maxNumberOfNonSpecialRooms++;
                 break;
         }
         return newR;
     }
 
+    public RoomNode Get(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.up:
+                return Up;
+            case Direction.down:
+                return Down;
+            case Direction.left:
+                return Left;
+            case Direction.right:
+                return Right;
+        }
+        return null;
+    }
+
 }
 
-public class LayoutGenerator : MonoBehaviour
+public  class LayoutGenerator : MonoBehaviour
 {
-    public List<RoomNode> rooms;
-    public List<Vector2> positionsVisited;
+    public Dictionary<Vector2Int,RoomNode> rooms;
+    public List<RoomNode> roomList;
+
+    public int numberOfBossRooms = 0;
+    public int maxNumberOfBossRooms;
+    public int numberOfLootRooms = 0;
+    public int maxNumberOfLootRooms;
+    public int numberOfNonSpecialRooms = 0;
+    public int maxNumberOfNonSpecialRooms;
+    public int maxNumberOfRooms;
+    private List<RoomGenerationRule> rules;
+    public DungeonGenerationData Dgd;
 
     void Start()
     {
+        maxNumberOfBossRooms = Dgd.numberOfBossRooms;
+        maxNumberOfLootRooms = Dgd.numberOfLootRooms;
+        maxNumberOfRooms = Mathf.RoundToInt(Random.Range(Dgd.minNumberOfRooms - 0.5f, Dgd.maxNumberOfRooms + 0.5f));
+        maxNumberOfNonSpecialRooms = MaxNumberOfNonSpecialRooms();
         InitializeRooms();
+        InitializeRules();
         GenerateLayout();
     }
 
-    public void InitializeRooms()
+    private int MaxNumberOfNonSpecialRooms()
     {
-        rooms = new List<RoomNode>
-        {
-            new RoomNode(RoomType.Start, new Vector2Int(0,0))
-        };
+        return maxNumberOfRooms - maxNumberOfLootRooms - maxNumberOfBossRooms;
     }
 
-    public List<RoomNode> GetLayout()
+    private void InitializeRooms()
+    {
+        rooms = new Dictionary<Vector2Int, RoomNode>();
+        roomList = new List<RoomNode>();
+        RoomNode start = new RoomNode(RoomType.Start, new Vector2Int(0, 0));
+        start.Lg = this;
+
+        rooms.Add(new Vector2Int(0, 0), start);
+        roomList.Add(start);
+    }
+
+    public Dictionary<Vector2Int, RoomNode> GetLayout()
     {
         return rooms;
     }
 
-    public void GenerateLayout()
+    /*
+     * Hier werden die Regeln angewandt um neue Räume zu generieren.
+     * Ein zufälliger existierender Raum wird ausgesucht. Es wird geprüft welche Regeln auf diesen Raum anwendbar sind.
+     * Wenn es keine gibt wird ein anderer zufälliger Raum ausgesucht. Wenn es keine Räume mit anwendbaren Regeln gibt bricht es ab.
+     * Wenn die zufällige Regel auf den zufälligen Raum anwendbar ist wird sie angewandt und das ganze wiederholt sich.
+     */
+    private void GenerateLayout()
     {
+        List<RoomGenerationRule> applicableRules;
+        List<RoomNode> workableRooms = new List<RoomNode>();
+        RoomGenerationRule randomRule;
+        foreach (RoomNode rn in roomList)
+        {
+            workableRooms.Add(rn);
+        }
+        while (workableRooms.Count > 0) {
+            RoomNode randomRoom = workableRooms[Mathf.RoundToInt(Random.Range(-0.5f, workableRooms.Count - 0.5f))];
+            applicableRules = new List<RoomGenerationRule>();
+            foreach (RoomGenerationRule rule in rules)
+            {
+                if (rule.IsApplicable(randomRoom))
+                {
+                    applicableRules.Add(rule);
+                }
+            }
+            if (applicableRules.Count == 0)
+            {
+                workableRooms.Remove(randomRoom);
+            }
+            else
+            {
+                randomRule = applicableRules[Mathf.RoundToInt(Random.Range(-0.5f, applicableRules.Count - 0.5f))];
+                randomRule.Apply(randomRoom);
+                workableRooms = new List<RoomNode>();
+                foreach (RoomNode rn in roomList)
+                {
+                    workableRooms.Add(rn);
+                }
+            }
+        }
+    }
+    
+    private void InitializeRules()
+    {
+        rules = new List<RoomGenerationRule>();
+        rules.Add(new Rule1());
+        rules.Add(new Rule2());
+        rules.Add(new Rule3());
+        rules.Add(new Rule4());
+        rules.Add(new Rule5());
+        rules.Add(new Rule6());
+        rules.Add(new Rule7());
+        rules.Add(new Rule8());
+        rules.Add(new Rule9());
+        rules.Add(new Rule10());
+        rules.Add(new Rule11());
+        rules.Add(new Rule12());
+        rules.Add(new Rule13());
+        rules.Add(new Rule14());
+        rules.Add(new Rule15());
+        rules.Add(new Rule16());
+        rules.Add(new Rule17());
+        rules.Add(new Rule18());
+        rules.Add(new Rule19());
+        rules.Add(new Rule20());
 
+        foreach(RoomGenerationRule rule in rules)
+        {
+            rule.Lg = this;
+            //print(rule.ToString());
+        }
     }
 
 }
